9640619e7669338413d2f80fd4aa09bfe8e6f5ba
Keitar6 <104099101+Keitar6@users.noreply.github.com>
2022-06-05 10:20:30 +0200

    Form_Modif

--- My_Website/Website/Contact/CodeFiles/script.js
+++ My_Website/Website/Contact/CodeFiles/script.js
@@ -23,76 +23,76 @@ listen(document, "mouseup touchend").start(() => {
 });
 // Spring logo end
 
-var form = document.getElementsByTagName("form")[0];
-form.addEventListener("submit", function (e) {
-	e.preventDefault();
-	sendData();
-});
+// var form = document.getElementsByTagName("form")[0];
+// form.addEventListener("submit", function (e) {
+// 	e.preventDefault();
+// 	sendData();
+// });
 
 // https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
-function sendData() {
-	var XHR = new XMLHttpRequest();
-	var urlEncodedData = "";
-	var urlEncodedDataPairs = [];
+// function sendData() {
+// 	var XHR = new XMLHttpRequest();
+// 	var urlEncodedData = "";
+// 	var urlEncodedDataPairs = [];
 
-	urlEncodedDataPairs.push(
-		encodeURIComponent("name") +
-			"=" +
-			encodeURIComponent(form.querySelector("[name='name']").value)
-	);
-	urlEncodedDataPairs.push(
-		encodeURIComponent("send_to") +
-			"=" +
-			encodeURIComponent(form.querySelector("[name='send_to']").value)
-	);
-	urlEncodedDataPairs.push(
-		encodeURIComponent("subject") +
-			"=" +
-			encodeURIComponent(form.querySelector("[name='subject']").value)
-	);
-	urlEncodedDataPairs.push(
-		encodeURIComponent("email") +
-			"=" +
-			encodeURIComponent(form.querySelector("[name='email']").value)
-	);
+// 	urlEncodedDataPairs.push(
+// 		encodeURIComponent("name") +
+// 			"=" +
+// 			encodeURIComponent(form.querySelector("[name='name']").value)
+// 	);
+// 	urlEncodedDataPairs.push(
+// 		encodeURIComponent("send_to") +
+// 			"=" +
+// 			encodeURIComponent(form.querySelector("[name='send_to']").value)
+// 	);
+// 	urlEncodedDataPairs.push(
+// 		encodeURIComponent("subject") +
+// 			"=" +
+// 			encodeURIComponent(form.querySelector("[name='subject']").value)
+// 	);
+// 	urlEncodedDataPairs.push(
+// 		encodeURIComponent("email") +
+// 			"=" +
+// 			encodeURIComponent(form.querySelector("[name='email']").value)
+// 	);
 
-	// dropdown menu
-	urlEncodedDataPairs.push(
-		encodeURIComponent("comments") +
-			"=" +
-			encodeURIComponent(form.querySelector("[name='comments']").value)
-	);
-	// Combine the pairs into a single string and replace all %-encoded spaces to
-	// the '+' character; matches the behaviour of browser form submissions.
-	urlEncodedData = urlEncodedDataPairs.join("&").replace(/%20/g, "+");
+// 	// dropdown menu
+// 	urlEncodedDataPairs.push(
+// 		encodeURIComponent("comments") +
+// 			"=" +
+// 			encodeURIComponent(form.querySelector("[name='comments']").value)
+// 	);
+// 	// Combine the pairs into a single string and replace all %-encoded spaces to
+// 	// the '+' character; matches the behaviour of browser form submissions.
+// 	urlEncodedData = urlEncodedDataPairs.join("&").replace(/%20/g, "+");
 
-	// Define what happens on successful data submission
-	XHR.addEventListener("load", function (event) {
-		if (XHR.readyState === XHR.DONE) {
-			if (XHR.status === 200) {
-				alert("Thanks for writing to me! I will come Back to you shortly!");
-			} else {
-				alert(
-					"Um, sorry, i am still working on it. Here is a problem --> " +
-						XHR.responseText +
-						"."
-				);
-			}
-		}
-	});
+// 	// Define what happens on successful data submission
+// 	XHR.addEventListener("load", function (event) {
+// 		if (XHR.readyState === XHR.DONE) {
+// 			if (XHR.status === 200) {
+// 				alert("Thanks for writing to me! I will come Back to you shortly!");
+// 			} else {
+// 				alert(
+// 					"Um, sorry, i am still working on it. Here is a problem --> " +
+// 						XHR.responseText +
+// 						"."
+// 				);
+// 			}
+// 		}
+// 	});
 
-	// Define what happens in case of error
-	XHR.addEventListener("error", function (event) {
-		// This is normally a timeout or connection error.
-		alert("Oops! Something went wrong.");
-	});
+// 	// Define what happens in case of error
+// 	XHR.addEventListener("error", function (event) {
+// 		// This is normally a timeout or connection error.
+// 		alert("Oops! Something went wrong.");
+// 	});
 
-	// Set up our request
-	XHR.open(form.getAttribute("method"), form.getAttribute("action"));
+// 	// Set up our request
+// 	XHR.open(form.getAttribute("method"), form.getAttribute("action"));
 
-	// Add the required HTTP header for form data POST requests
-	// XHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// 	// Add the required HTTP header for form data POST requests
+// 	// XHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-	// Finally, send our data.
-	XHR.send(urlEncodedData);
-}
+// 	// Finally, send our data.
+// 	XHR.send(urlEncodedData);
+// }
